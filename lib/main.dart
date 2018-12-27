a87e9687326f6614ea0870ab22d4a6ea5eb5b15c
Carlos Levir Freire de Souza <carlos.levir@secrelnet.local>
2018-12-27 15:09:31 -0300

    Build of Deletion Task Code

--- lib/main.dart
+++ lib/main.dart
@@ -25,60 +25,89 @@ class TodoListState extends State<TodoList> {
     return new ListView.builder(
       itemBuilder: (context, index) {
         if(index < _todoItems.length) {
-          return _buildTodoItem(_todoItems[index]);
+          return _buildTodoItem(_todoItems[index], index);
         }
       },
     );
   }
 
-  Widget _buildTodoItem(String todoText) {
+  Widget _buildTodoItem(String todoText, int index) {
     return new ListTile(
-      title: new Text(todoText)
+      title: new Text(todoText),
+      onTap: () => _promptRemoveTodoItem(index)
+    );
+  }
+
+  void _removeTodoItem(int index) {
+    setState(() => _todoItems.removeAt(index));
+  }
+
+  void _promptRemoveTodoItem(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return new AlertDialog(
+          title: new Text('Mark "${_todoItems[index]}" as done?'),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text('CANCEL'),
+              onPressed: () => Navigator.of(context).pop()
+            ),
+            new FlatButton(
+              child: new Text('MARK AS DONE'),
+              onPressed: () {
+                _removeTodoItem(index);
+                Navigator.of(context).pop();
+              }
+            )
+          ]
+        );
+      }
     );
   }
 
   void _addTodoItem(String task) {
-  if(task.length > 0) {
-    setState(() => _todoItems.add(task));
+    if(task.length > 0) {
+      setState(() => _todoItems.add(task));
+    }
   }
-}
 
-Widget build(BuildContext context) {
-  return new Scaffold(
-    appBar: new AppBar(
-      title: new Text('Todo List')
-    ),
-    body: _buildTodoList(),
-    floatingActionButton: new FloatingActionButton(
-      onPressed: _pushAddTodoScreen,
-      tooltip: 'Add task',
-      child: new Icon(Icons.add)
-    ),
-  );
-}
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Todo App')
+      ),
+      body: _buildTodoList(),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _pushAddTodoScreen,
+        tooltip: 'Add task',
+        child: new Icon(Icons.add)
+      ),
+    );
+  }
 
-void _pushAddTodoScreen() {
-  Navigator.of(context).push(
-    new MaterialPageRoute(
-      builder: (context) {
-        return new Scaffold(
-          appBar: new AppBar(
-            title: new Text('Add a new task')
-          ),
-          body: new TextField(
-            autofocus: true,
-            onSubmitted: (val) {
-              _addTodoItem(val);
-              Navigator.pop(context);
-            },
-            decoration: new InputDecoration(
-              hintText: 'Enter something to do...',
-              contentPadding: const EdgeInsets.all(16.0)
+  void _pushAddTodoScreen() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Add a new task')
             ),
-          )
-        );
-      }
-    )
-  );
-}
+            body: new TextField(
+              autofocus: true,
+              onSubmitted: (val) {
+                _addTodoItem(val);
+                Navigator.pop(context);
+              },
+              decoration: new InputDecoration(
+                hintText: 'Enter the new task',
+                contentPadding: const EdgeInsets.all(16.0)
+              ),
+            )
+          );
+        }
+      )
+    );
+  }
 }
